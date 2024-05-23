166c52c1758c791d27407d8f34910f8a65164802
TamiraAtieno1 <tamiraatieno20@gmail.com>
2024-05-23 07:41:01 +0300

    updates

--- Javascript solutions/reverseVowels.js
+++ Javascript solutions/reverseVowels.js
@@ -2,10 +2,10 @@
  * Given a string s, reverse only all the vowels in the string and return it.
    The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once
  */
-
+  // helper functions
    function reverseVowels(s){
     // Creating a Set to store all possible outcomes of the vowels both capital and small
-    const vowels = new Set ([a, e, i, o, u, A, E, I, O, U]);
+    const vowels = new Set (['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
     //  split function separates the string into an array
     let sArray = s.split("");
 
